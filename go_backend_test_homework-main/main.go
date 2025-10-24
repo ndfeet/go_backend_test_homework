e9c8941266558592da2fcd16dab1b1a21c5a465e
ndfeet <ruslanbekunusov001@gmail.com>
2025-10-24 23:24:39 +0300

    омментарий

--- go_backend_test_homework-main/main.go
+++ go_backend_test_homework-main/main.go
@@ -9,9 +9,9 @@ func Add(a, b int) int {
 func main() {
 
 	fmt.Println("Я домашка")
-	fmt.Println("Я домашка")
-	fmt.Println("Я домашка")
-	fmt.Println("Я домашка")
+	fmt.Println("Я не домашка")
+	fmt.Println("Я от домашкаи")
+	fmt.Println("Я же домашка")
 }
 
 //rkiejrierjiejr
